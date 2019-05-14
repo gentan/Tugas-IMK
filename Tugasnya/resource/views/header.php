cdc66830a220fc10aa1fc03ca7be4315db5ce50f
Farhan027 <50628382+Farhan027@users.noreply.github.com>
2019-05-14 22:05:40 +0700

    Nama headernya salah gayn

--- Tugasnya/resource/views/header.php
+++ Tugasnya/resource/views/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="<?= $GLOBALS['assets'] ?>/css/style.css">
 </head>
 <body>
-	<div class="col-12 header">Siswa Pendukung Keputusan Rekomendasi Program Studi Universitas</div>
+	<div class="col-12 header">Sistem Pendukung Keputusan Rekomendasi Program Studi di Universitas</div>
 	    <div class="col-8 col-sm-12 nav offset-3">
 	        <a href="<?= url('') ?>" class="parallelogram col-1 col-md-6 col-sm-12">Home</a>
 	        <a href="<?= url('daftarSiswa') ?>" class="parallelogram col-2 col-md-6 col-sm-12">Daftar Siswa</a>
